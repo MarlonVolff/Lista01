a65ba79523619e9a083adbfd5742c758b012d97b
MarlonVolff <116438923+MarlonVolff@users.noreply.github.com>
2024-03-23 00:00:28 -0300

    Exercicio02.java
    Arrumando erros.

--- Exercicio02.java
+++ Exercicio02.java
@@ -1,15 +1,14 @@
 import java.util.Scanner;
 
 public class Exercicio02 {
-
     public static void executar(){
         Scanner leitor = new Scanner(System.in);
 
-        System.out.print("Digite o primiro número: ");
+        System.out.print("Digite o primero número: ");
         int num1 = leitor.nextInt();
-
         System.out.print("Digite o segundo número: ");
         int num2 = leitor.nextInt();
+        
         int sum = num1 + num2;
         System.out.printf("A soma é: "+  num1 + num2 + sum);
 
