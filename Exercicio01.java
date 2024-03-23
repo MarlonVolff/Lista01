0d582f33d471bd28f8297c0547495b40ff3a5c99
MarlonVolff <116438923+MarlonVolff@users.noreply.github.com>
2024-03-22 23:54:15 -0300

    Exercicio01
    Arrumado erros de digitação

--- Exercicio01.java
+++ Exercicio01.java
@@ -1,16 +1,14 @@
 import java.util.Scanner;
-
 public class Exercicio01 {
 
     public static void executar() {  
-
         Scanner leitor = new Scanner(System.in);
         System.out.println("Digite um número: ");
         int num = leitor.nextInt();
         if (num > 10) {
             System.out.println("Número maior do que 10!");
         } else {
-            System.out.println("Número maior ou iual a 10!");
+            System.out.println("Número menor ou igual a 10!");
         }
         leitor.close();
     }
