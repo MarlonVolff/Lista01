ddd7c8c1bf54e434b6e1e3df1ab5518840cb53e0
MarlonVolff <116438923+MarlonVolff@users.noreply.github.com>
2024-03-23 00:07:06 -0300

    Exercicio06
    arrumando os nome do commit

--- Exercicio06.java
+++ Exercicio06.java
@@ -4,10 +4,9 @@ public class Exercicio06 {
     public static void executar(){
         Scanner leitor = new Scanner(System.in);
 
-        System.out.println("Digite a temperatura em graus Celsius: ");
+        System.out.println("Informe a temperatura em graus Celsius: ");
         int celsius = leitor.nextInt();
         int fahrenheit = (9 * celsius + 160) / 5;
-
         System.out.println("A temperatura em Fahrenheit é: " + fahrenheit);
 
         leitor.close();
