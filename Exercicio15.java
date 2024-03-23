8b4583c5ab38f5fe052edd3953e400b50312c830
MarlonVolff <116438923+MarlonVolff@users.noreply.github.com>
2024-03-23 00:27:55 -0300

    Exercicio15
    Simplificando o código

--- Exercicio15.java
+++ Exercicio15.java
@@ -4,16 +4,16 @@ public class Exercicio15{
     public static void executar(){
         Scanner  leitor = new Scanner(System.in);
 
-        System.out.println("Digite as coordenadas do ponto P1 (x1 y1):");
+        System.out.println("Informe as coordenadas do ponto P1:");
         int x1 = leitor.nextInt();
         int y1 = leitor.nextInt();
-        System.out.println("Digite as coordenadas do ponto P2 (x2 y2):");
+        System.out.println("Informe as coordenadas do ponto P2:");
         int x2 = leitor.nextInt();
         int y2 = leitor.nextInt();
 
         int distancia = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
 
-        System.out.printf("Distância entre os pontos: %.2f\n", distancia);
+        System.out.printf("Distância entre os pontos: " + distancia);
 
 
         leitor.close();
